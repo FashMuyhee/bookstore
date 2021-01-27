58281e40adf7a1a008d2aee43697881375987c7a
FashMuyhee <fashmuyidavid@gmail.com>
2021-01-27 21:24:10 +0100

    modified with actual design

--- src/screens/Book.jsx
+++ src/screens/Book.jsx
@@ -1,14 +1,151 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, {useState} from 'react';
+import {StyleSheet, View, Image, TouchableWithoutFeedback} from 'react-native';
+import {ScrollContainer, NavBar} from '../components';
+import {Text, Button} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/Feather';
+import book_2 from '../assets/img/book_2.png';
+import message from '../assets/img/message.png';
+import fileText from '../assets/img/file-text.png';
+import {Rating} from 'react-native-ratings';
 
-const Book = () => {
-    return (
-        <View>
-            <Text>Book</Text>
+const Book = ({navigation}) => {
+  const [rating, setRating] = useState(2.3);
+
+  return (
+    <>
+      <View style={styles.navBar}>
+        <NavBar
+          bgColor="white"
+          left={
+            <Icon
+              name="chevron-left"
+              size={30}
+              onPress={() => navigation.goBack()}
+            />
+          }
+          right={
+            <View
+              style={{
+                flexDirection: 'row',
+                width: 80,
+                justifyContent: 'space-between',
+              }}>
+              <Icon name="bookmark" color="black" size={30} />
+              <Icon name="more-vertical" color="black" size={30} />
+            </View>
+          }
+        />
+      </View>
+      <ScrollContainer>
+        <View style={styles.product}>
+          <Image source={book_2} style={{width: 230, height: 330}} />
+          <View style={styles.name}>
+            <Text
+              style={{fontSize: 30, fontWeight: 'bold', textAlign: 'center'}}>
+              Book Name
+            </Text>
+            <Text style={{textAlign: 'center', color: 'grey', fontSize: 18}}>
+              Fajuyi Bose
+            </Text>
+          </View>
+          <View style={styles.ratingContainer}>
+            <Rating
+              type="star"
+              ratingCount={5}
+              imageSize={20}
+              style={{
+                flexDirection: 'row-reverse',
+                alignItems: 'flex-start',
+              }}
+              ratingTextColor="black"
+              startingValue={rating}
+              isDisabled
+              fractions={2}
+            />
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'flex-start',
+              }}>
+              <Text style={styles.ratingText}>{rating}</Text>
+              <Text style={{...styles.ratingText, color: 'grey'}}>/5.0</Text>
+            </View>
+          </View>
         </View>
-    )
-}
+        <View>
+          <Text style={{fontSize: 19, color: 'grey'}}>
+            A spectacular visual journey through 40 years of haute couture from
+            one of the best-known and most trend-setting brands in fashion.
+          </Text>
+          <View style={styles.btnGrp}>
+            <TouchableWithoutFeedback>
+              <View style={styles.btn}>
+                <Image source={fileText} />
+                <Text style={{fontSize: 20}}>Preview</Text>
+              </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback>
+              <View style={styles.btn}>
+                <Image source={message} />
+                <Text style={{fontSize: 20}}>Reviews</Text>
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
+          <Button
+            mode="contained"
+            style={{height: 60, marginTop: 30}}
+            labelStyle={{
+              fontSize: 20,
+              paddingVertical: 8,
+            }}
+            uppercase={false}>
+            Buy Now for $46.77
+          </Button>
+        </View>
+      </ScrollContainer>
+    </>
+  );
+};
 
-export default Book
+export default Book;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  navBar: {
+    paddingHorizontal: 20,
+    backgroundColor: '#FDFDFD',
+  },
+  product: {
+    alignItems: 'center',
+    height: 480,
+  },
+  name: {
+    marginVertical: 20,
+    textAlign: 'center',
+  },
+  ratingText: {
+    fontSize: 20,
+  },
+  ratingContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    display: 'flex',
+    width: 180,
+    height: 35,
+  },
+  btn: {
+    backgroundColor: 'white',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flexDirection: 'row',
+    width: '48%',
+    elevation: 5,
+    height: 40,
+    borderRadius: 8,
+  },
+  btnGrp: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginVertical: 15,
+  },
+});
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,14 +1,61 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import {StyleSheet, Image, View, StatusBar} from 'react-native';
+import {NavBar, ScrollContainer, BookCard, BookList} from '../components';
+import user from '../assets/img/user.png';
+import Icon from 'react-native-vector-icons/Feather';
+import {Text, Title} from 'react-native-paper';
+import book_1 from '../assets/img/book_1.png';
+import book_2 from '../assets/img/book_2.png';
+import book_3 from '../assets/img/book_3.png';
 
 const Home = () => {
-    return (
-        <View>
-            <Text>Home</Text>
+  const [state, setstate] = React.useState([
+    {title: 'Movie 1', id: 1, img: book_1, author: 'Feddrick Ben'},
+    {title: 'Movie 2', id: 2, img: book_2, author: 'Elon Musk'},
+    {title: 'Movie 3', id: 3, img: book_3, author: 'Ali baba'},
+  ]);
+  return (
+    <>
+          <StatusBar backgroundColor="white" barStyle="dark-content" />
+
+      <View style={styles.navBar}>
+        <NavBar
+          bgColor="white"
+          left={<Image source={user} style={styles.avatar} />}
+          center={
+            <Text style={{fontSize: 20, fontWeight: '600'}}>Hi Dustin!</Text>
+          }
+          right={<Icon name="search" color="black" size={30} />}
+        />
+      </View>
+      <ScrollContainer>
+        <View style={styles.bookContainer}>
+          <Text style={styles.title}>Popular Books </Text>
+          <BookCard data={state} />
         </View>
-    )
-}
+        <View style={{...styles.bookContainer, height: '50%'}}>
+          <Text style={styles.title}>Newest </Text>
+          <BookList data={state} />
+        </View>
+      </ScrollContainer>
+    </>
+  );
+};
 
-export default Home
+export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  avatar: {width: 60, height: 60, borderRadius: 15},
+  title: {
+    fontSize: 40,
+    fontWeight: '600',
+  },
+  bookContainer: {
+    height: 370,
+    paddingBottom: 20,
+  },
+  navBar: {
+    padding: 20,
+    backgroundColor: '#FDFDFD',
+  },
+});
