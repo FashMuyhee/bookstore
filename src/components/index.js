fb41fa14c1fa08f8b84a3478e05bf92486dd066f
FashMuyhee <fashmuyidavid@gmail.com>
2021-01-29 22:12:10 +0100

    added tabbar component

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ import NavBar from './NavBar';
 import BookCard from './BookCard';
 import Input from './Input';
 import BookList from './BookList';
+import TabBar from './TabBar';
 
-export {Container, ScrollContainer, BookCard, NavBar, Input, BookList};
+export {Container, ScrollContainer, BookCard, NavBar, Input, BookList, TabBar};
